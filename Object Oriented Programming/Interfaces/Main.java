e3f5302bb5d5f7ff82a7165635089302c71c7104
KARTHIK V T <vtkarthik22@gmail.com>
2025-09-29 14:36:41 +0530

    Update Main.java

--- Object Oriented Programming/Interfaces/Main.java
+++ Object Oriented Programming/Interfaces/Main.java
@@ -1,1 +1,12 @@
+//package OOPS.packages.inter_faces;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle c1 = new Car ();
+        c1.start();
+        c1.acc();
+        c1.stop();
+        //c1.brake();
+    }
+}
 
