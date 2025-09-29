436fdef8f4f0d1f99e5a8d03d6ccf2603e37b6dd
KARTHIK V T <vtkarthik22@gmail.com>
2025-09-29 14:41:57 +0530

    Create shpaes.java

---
+++ Object Oriented Programming/Polymorphisim/shpaes.java
@@ -0,0 +1,12 @@
+//package OOPS.packages.polymorphisim;
+
+public class shapes {
+     private double l= 12.5;
+    static void print(){
+       
+    System.out.println("i am in shapes dude");
+    }
+    public double getl(){
+        return l;
+    }
+}
